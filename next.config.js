af2f148d94eb61b8c730c48a329f5dc258313077
Jeff Chew <jeff.chew@gmail.com>
2020-08-25 12:57:16 -0400

    chore(expressivetheme): updating to include expressive theme
    This change will enable the CSS Custom Properties flag in order for the
    expressive theme to work. In addition, the progress bar was removed from
     webpack builds as it is noted it can slow down the build times. Some
     other minor updates were made as well, namely updating imports for
     styles to the updated location.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const path = require("path");
-const withCSS = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
-const withProgressBar = require("next-progressbar");
 
 const NODE_ENV = "development";
 
@@ -26,7 +24,7 @@ const sassLoader = {
     includePaths: [path.resolve(__dirname, "node_modules")],
     data: `
       $feature-flags: (
-        ui-shell: true,
+        enable-css-custom-properties: true
       );
     `,
     sourceMap: true,
@@ -39,35 +37,31 @@ const fastSassLoader = {
     includePaths: [path.resolve(__dirname, "node_modules")],
     data: `
     $feature-flags: (
-      ui-shell: true,
+      enable-css-custom-properties: true
     );
   `,
     sourceMap: true,
   },
 };
 
-module.exports = withProgressBar(
-  withSass(
-    withCSS({
-      env: {
-        CORS_PROXY: process.env.CORS_PROXY || "",
-        ROOT_PATH: process.env.ROOT_PATH || "/",
-      },
-      sassLoaderOptions: {
-        includePaths: [path.resolve(__dirname, "node_modules")],
-      },
-      webpack: (config) => {
-        config.module.rules.push({
-          test: /\.scss$/,
-          sideEffects: true,
-          use: [
-            ...styleLoaders,
-            NODE_ENV === "production" ? sassLoader : fastSassLoader,
-          ],
-        });
+module.exports = withSass({
+  env: {
+    CORS_PROXY: process.env.CORS_PROXY || "",
+    ROOT_PATH: process.env.ROOT_PATH || "/",
+  },
+  sassLoaderOptions: {
+    includePaths: [path.resolve(__dirname, "node_modules")],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      sideEffects: true,
+      use: [
+        ...styleLoaders,
+        NODE_ENV === "production" ? sassLoader : fastSassLoader,
+      ],
+    });
 
-        return config;
-      },
-    })
-  )
-);
+    return config;
+  },
+});
