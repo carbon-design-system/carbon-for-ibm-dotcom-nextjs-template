15b7d73a05c98f14103419cb51aca912eee24d25
Jeff Chew <jeff.chew@gmail.com>
2021-08-03 16:18:39 -0400

    feat(next): upgrade to next 11 (WIP)

--- next.config.js
+++ next.config.js
@@ -1,66 +1,26 @@
 require("dotenv").config();
 const path = require("path");
-const withSass = require("@zeit/next-sass");
 const rtlcss = require("rtlcss");
 
-module.exports = withSass({
+const postcssPlugins = [];
+
+if (process.env.ENABLE_RTL === "true") {
+  postcssPlugins.push(rtlcss);
+}
+
+module.exports = {
   assetPrefix: ".",
   basePath: process.env.BASE_PATH || "",
   env: {
     ALTLANG_ROOT_PATH: process.env.ALTLANG_ROOT_PATH || "/",
     ENABLE_RTL: process.env.ENABLE_RTL || "false",
   },
-  sassLoaderOptions: {
-    sassOptions: {
-      includePaths: [path.resolve(__dirname, "node_modules")],
-    },
+  sassOptions: {
+    includePaths: [path.resolve(__dirname, "node_modules")],
+    additionalData: `
+      $feature-flags: (
+        enable-css-custom-properties: true
+      );
+    `,
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      sideEffects: true,
-      use: [
-        {
-          loader: "postcss-loader",
-          options: {
-            plugins: () => {
-              const autoPrefixer = require("autoprefixer")({
-                overrideBrowserslist: ["last 1 version", "ie >= 11"],
-              });
-              return process.env.ENABLE_RTL === "true"
-                ? [autoPrefixer, rtlcss]
-                : [autoPrefixer];
-            },
-          },
-        },
-        {
-          loader:
-            process.env.NODE_ENV === "production"
-              ? "sass-loader"
-              : "fast-sass-loader",
-          options: Object.assign(
-            process.env.NODE_ENV === "production"
-              ? {
-                  sassOptions: {
-                    includePaths: [path.resolve(__dirname, "node_modules")],
-                  },
-                }
-              : {
-                  includePaths: [path.resolve(__dirname, "node_modules")],
-                },
-            {
-              additionalData: `
-              $feature-flags: (
-                enable-css-custom-properties: true
-              );
-            `,
-              sourceMap: process.env.NODE_ENV !== "production",
-            }
-          ),
-        },
-      ],
-    });
-
-    return config;
-  },
-});
+};
